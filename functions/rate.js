7ab049824932292ecd5787fa9285cbcc51fb647c
Natalia Sergeeva <natalia.m.sergeeva@gmail.com>
2025-07-12 18:13:05 +0300

    Fix request params

--- functions/rate.js
+++ functions/rate.js
@@ -1,7 +1,10 @@
 export async function onRequest(context) {
   const API_KEY = context.env.BYBIT_API_KEY;
   const API_SECRET = context.env.BYBIT_API_SECRET;
-  
+
+  var recvWindow = 5000;
+  var timestamp = Date.now().toString();
+
   // https://bybit-exchange.github.io/docs/p2p/ad/online-ad-list#request-parameters 
   // Параметры запроса
   const paramsObj = {
@@ -18,22 +21,23 @@ export async function onRequest(context) {
   // Формируем строку для подписи: timestamp + api_key + recvWindow + body
   const signPayload = `${timestamp}${API_KEY}${recvWindow}${body}`;
 
-  // HMAC SHA256 через Web Crypto API
-  async function sign(secret, payload) {
-    const enc = new TextEncoder();
-    const key = await crypto.subtle.importKey(
-      'raw',
-      enc.encode(secret),
-      { name: 'HMAC', hash: 'SHA-256' },
-      false,
-      ['sign']
-    );
-    const signature = await crypto.subtle.sign('HMAC', key, enc.encode(payload));
-    return Array.from(new Uint8Array(signature)).map(b => b.toString(16).padStart(2, '0')).join('');
-  }
+  // // HMAC SHA256 через Web Crypto API
+  // async function sign(secret, payload) {
+  //   const enc = new TextEncoder();
+  //   const key = await crypto.subtle.importKey(
+  //     'raw',
+  //     enc.encode(secret),
+  //     { name: 'HMAC', hash: 'SHA-256' },
+  //     false,
+  //     ['sign']
+  //   );
+  //   const signature = await crypto.subtle.sign('HMAC', key, enc.encode(payload));
+  //   return Array.from(new Uint8Array(signature)).map(b => b.toString(16).padStart(2, '0')).join('');
+  // }
 
   try {
-    const signature = await sign(API_SECRET, signPayload);
+    //const signature = await sign(API_SECRET, signPayload);
+    const signature = await getSignature(signPayload, API_SECRET);
 
     const response = await fetch(url, {
       method: "POST",
@@ -83,4 +87,8 @@ export async function onRequest(context) {
       headers: { "Content-Type": "text/plain; charset=utf-8" }
     });
   }
+}
+
+function getSignature(parameters, secret) {
+    return crypto.createHmac('sha256', secret).update(timestamp + API_KEY + recvWindow + parameters).digest('hex');
 }
